ead9b2d641d4c18c454ce4d4c54bbbe2542b5dad
Xiao <756044579@qq.com>
2023-10-09 14:17:54 +0800

    add streamlit webui

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ cdk provision Amazon connect resources example
 
 #### Build CDK env
 ```bash
+pip install streamlit
 npm install -g aws-cdk
 cdk bootstrap aws://ACCOUNT-NUMBER/REGION
 git clone https://github.com/micxyj/connect-cdk.git
@@ -11,17 +12,15 @@ cd connect-cdk-example
 cdk init app --language python
 python -m pip install -r requirements.txt
 mv ../connect-cdk/connect_cdk_stack.py ./connect_cdk_example/connect_cdk_example_stack.py
+mv ../connect-cdk/app.py ./app.py
+mv ../connect-cdk/logo.png ./logo.png
 ```
 
-#### Update connect_cdk_stack.py 
+#### Update connect_cdk_example_stack.py 
 Fill your parameters of connect
 
-#### Deploying stack
+#### Running Streamlit APP
 ```bash
-cdk deploy
-```
-
-#### Destroying stack
-```bash
-cdk destroy
+cd connect_cdk_example
+streamlit run connect_cdk_example_stack.py
 ```
--- connect_cdk_stack.py
+++ connect_cdk_stack.py
@@ -6,29 +6,129 @@ from aws_cdk import (
 )
 from constructs import Construct
 from aws_cdk import aws_connect as connect
+import os
+import subprocess
+import streamlit as st
+import pandas as pd
+import boto3
+import time
+from PIL import Image
 
-class ConnectCdkStack(Stack):
+
+# setting work dir
+# 需要填写绝对路径
+os.chdir('YOUR_PATH/connect-cdk-example/')
+
+
+# app title
+col1, col2 = st.columns([1, 10])
+image = Image.open('./logo.png')
+with col1:
+    st.image(image, use_column_width='always')
+with col2:
+    header = f"Amazon Connect Deployment Tool!"
+    st.write(f"<h3 class='main-header'>{header}</h3>", unsafe_allow_html=True)
+
+# add connect agents
+st.subheader('Agents Configuration', divider="rainbow")
+uploaded_file = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+    df.to_csv("agents.csv", index=False)
+
+# st.divider()
+
+# connect instance configuration
+st.subheader('Connect Parameters', divider="rainbow")
+connect_instance_arn = st.text_input('Amazon Connect instance ARN', value="")
+
+# security profile configuration
+security_profile_arn = st.text_input('Security profile ARN', value="")
+
+# hours of operation configuration
+hop_arn = st.text_input('Hours of operation ARN', value="")
+
+# hours of operation configuration
+tenant_name = st.text_input('Tenant Name (Required)')
+
+# save env
+st.write('*You must click follow button to save configuration*')
+if st.button('Save Configuration'):
+    os.environ["connect_instance_arn"] = connect_instance_arn
+    os.environ["security_profile_arn"] = security_profile_arn
+    os.environ["hop_arn"] = hop_arn
+    os.environ["tenant_name"] = tenant_name
+    st.success("ENV has been set")
+
+# st.divider()
+
+# deploy cdk
+st.subheader('CDK Deployment', divider="rainbow")
+if st.button('Deploy CDK Stack'):
+    subprocess.Popen(['cdk', 'deploy'])
+    st.write('CDK stack initialized...........')
+    time.sleep(5)
+    with st.spinner('Deploying......'):
+        cfm_client = boto3.client("cloudformation")
+        try:
+            while True:
+                time.sleep(5)
+                res = cfm_client.describe_stacks()
+                stacks = [i['StackName'] for i in res['Stacks']]
+                if os.environ["tenant_name"] in stacks:
+                    res = cfm_client.describe_stacks(StackName=os.environ["tenant_name"])
+                    status = res['Stacks'][0]['StackStatus']
+                    if status == 'CREATE_COMPLETE':
+                        st.success('Deploy complete!')
+                        break
+                    elif status in ['CREATE_FAILED', 'ROLLBACK_COMPLETE']:
+                        st.error('Deploy failed, please check CloudFormation event for detailed messages.')
+                        break
+                    else:
+                        continue
+        except Exception as e:
+            st.error('Failed')
+
+# destroy cdk
+st.subheader('Clean Resources', divider="rainbow")
+if st.button('Destroy CDK Stack'):
+    subprocess.Popen(['cdk', 'destroy', '--force'])
+    st.write('Destroying CDK stack...........')
+    time.sleep(5)
+    with st.spinner('Destroying......'):
+        cfm_client = boto3.client("cloudformation")
+        try:
+            while True:
+                time.sleep(5)
+                res = cfm_client.describe_stacks()
+                stacks = [i['StackName'] for i in res['Stacks']]
+                if os.environ["tenant_name"] not in stacks:
+                    st.success('Destroy complete!')
+                    break
+                else:
+                    res = cfm_client.describe_stacks(StackName=os.environ["tenant_name"])
+                    status = res['Stacks'][0]['StackStatus']
+                    if status == 'DELETE_FAILED':
+                        st.error('Destroy failed, please check CloudFormation event for detailed messages.')
+                        break
+                    else:
+                        continue
+        except Exception as e:
+            st.error('Failed')
+
+
+class ConnectCdkExampleStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # parameter
-        connect_instance_arn = ''
-        security_profile_arn = ''
-        hop_arn = ''
-        agents = [
-            {
-                "FirstName": "kevin",
-                "LastName": "li",
-                "Username": "kevin"
-            },
-            {
-                "FirstName": "felix",
-                "LastName": "wang",
-                "Username": "felix"
-            },
-        ]
+        connect_instance_arn = os.environ["connect_instance_arn"]
+        security_profile_arn = os.environ["security_profile_arn"]
+        hop_arn = os.environ["hop_arn"]
         count = 1
+        df = pd.read_csv("agents.csv")
 
         # define phone number
         cfn_phone_number = connect.CfnPhoneNumber(self, "CfnPhoneNumber",
@@ -45,10 +145,11 @@ class ConnectCdkStack(Stack):
         )
 
         # define IVR
+        # 需要填写 content 参数，入参为 connect flow
         cfn_contact_flow = connect.CfnContactFlow(self, "CfnContactFlow",
             content="",
             instance_arn=connect_instance_arn,
-            name="CfnFlow",
+            name=os.environ["tenant_name"]+"CfnFlow",
             type="CONTACT_FLOW",
 
             # the properties below are optional
@@ -107,7 +208,7 @@ class ConnectCdkStack(Stack):
         )
 
         # define agents
-        for agent in agents:
+        for index, row in df.iterrows():
             cfn_user = connect.CfnUser(self, "CfnUser"+str(count),
                 instance_arn=connect_instance_arn,
                 phone_config=connect.CfnUser.UserPhoneConfigProperty(
@@ -118,12 +219,12 @@ class ConnectCdkStack(Stack):
                 ),
                 routing_profile_arn=cfn_routing_profile.attr_routing_profile_arn,
                 security_profile_arns=[security_profile_arn],
-                username=agent["Username"],
+                username=row["Username"],
 
                 # the properties below are optional
                 identity_info=connect.CfnUser.UserIdentityInfoProperty(
-                    first_name=agent["FirstName"],
-                    last_name=agent["LastName"],
+                    first_name=row["FirstName"],
+                    last_name=row["LastName"],
                 ),
                 password="Aa12345678.",
                 tags=[CfnTag(
